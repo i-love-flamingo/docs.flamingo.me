80e61cac85838aa57e1f192e4f721d33205c2194
Daniel Pötzinger <daniel.poetzinger@aoe.com>
2018-09-11 23:24:40 +0200

    add second example build

--- _build_other_assets.sh
+++ _build_other_assets.sh
@@ -2,10 +2,32 @@
 
 # Supposed to run as last build step
 
-echo "*****Clone the example projects"
+mkdir -p docs/site/examples/
+
+echo "*****Clone the example projects HELLOWORLD"
 cd workspace/example-helloworld
 git pull --all
 cd ..
 zip example-helloworld.zip example-helloworld
 mkdir -p docs/site/examples/
 cp example-helloworld.zip docs/site/examples/
+
+
+
+
+echo "*****Clone the example projects  FLAMINGO CAROTENE HELLO WORLD"
+
+cd example-hello-flamingo-carotene
+git pull --all
+
+cd frontend
+docker run --rm -w "/tmp" -v ${PWD}:/tmp aoepeople/carotene-env:1.7.0  "yarn"
+
+
+cd ../..
+zip example-hello-flamingo-carotene.zip example-hello-flamingo-carotene
+
+cp example-hello-flamingo-carotene.zip docs/site/examples/
+
+
+echo "DONE :-)"
--- _prepare-build.sh
+++ _prepare-build.sh
@@ -12,9 +12,12 @@ echo "*****Clone flamingo and flamingo commerce"
 git clone git@gitlab.aoe.com:shared/i-love-flamingo/flamingo.git workspace/flamingo
 git clone git@gitlab.aoe.com:shared/i-love-flamingo/flamingo-commerce.git workspace/flamingo-commerce
 git clone git@gitlab.aoe.com:shared/i-love-flamingo/example-helloworld.git workspace/example-helloworld
+git clone git@gitlab.aoe.com:shared/i-love-flamingo/example-hello-flamingo-carotene.git workspace/example-hello-flamingo-carotene
+
 
 echo "*****Copy package readmes to workspace"
 ./_copyPackageDocs.sh
+
 echo "*****Compiling documentation"
 cd workspace/docs
 cd ../..
