42ef62ac4db848f2c946e261383b358e520c1e5e
Daniel Pötzinger <daniel.poetzinger@aoe.com>
2018-09-12 22:54:43 +0200

    add example 2

--- _build_other_assets.sh
+++ _build_other_assets.sh
@@ -30,4 +30,21 @@ zip -r example-hello-flamingo-carotene.zip example-hello-flamingo-carotene
 cp example-hello-flamingo-carotene.zip docs/site/examples/
 
 
+
+
+echo "*****Clone the example projects  FLAMINGO CAROTENE HELLO WORLD"
+
+cd example-openweather
+git pull --all
+
+cd frontend
+docker run --rm -w "/tmp" -v ${PWD}:/tmp aoepeople/carotene-env:1.7.0  "yarn"
+
+
+cd ../..
+zip -r example-openweather.zip example-openweather
+
+cp example-openweather.zip docs/site/examples/
+
+
 echo "DONE :-)"
--- _prepare-build.sh
+++ _prepare-build.sh
@@ -13,6 +13,7 @@ git clone git@gitlab.aoe.com:shared/i-love-flamingo/flamingo.git workspace/flami
 git clone git@gitlab.aoe.com:shared/i-love-flamingo/flamingo-commerce.git workspace/flamingo-commerce
 git clone git@gitlab.aoe.com:shared/i-love-flamingo/example-helloworld.git workspace/example-helloworld
 git clone git@gitlab.aoe.com:shared/i-love-flamingo/example-hello-flamingo-carotene.git workspace/example-hello-flamingo-carotene
+git clone git@gitlab.aoe.com:shared/i-love-flamingo/example-openweather.git workspace/example-openweather
 
 
 echo "*****Copy package readmes to workspace"
