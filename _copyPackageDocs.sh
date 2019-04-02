3c0c9f196980d508b0a10509bbf632a321a625e5
Dumont, Dominik <dominik.dumont@aoe.com>
2019-04-02 10:23:23 +0200

    Update _copyPackageDocs.sh

--- _copyPackageDocs.sh
+++ _copyPackageDocs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # From flamingo
 cp "workspace/flamingo/docs/0. Introductions/1. Getting Started.md" "workspace/docs/docs/0. Introduction/"
 cp -R "workspace/flamingo/docs/1. Flamingo Basics" "workspace/docs/docs/1. Flamingo/"
@@ -15,9 +14,6 @@ cp -R -L "workspace/flamingo-commerce/docs/2. Flamingo Commerce Modules" "worksp
 # From example-helloworld
 cp "workspace/example-helloworld/Readme.md" "workspace/docs/docs/0. Introduction/2. Tutorial Hello World.md"
 
-
 # From Carotene
 cp -R -L "workspace/flamingo-carotene/docs/1. Flamingo Carotene Basics" "workspace/docs/docs/2. Flamingo Carotene/"
 cp -R -L "workspace/flamingo-carotene/docs/2. Flamingo Carotene Modules" "workspace/docs/docs/2. Flamingo Carotene/"
-cp -R -L "workspace/flamingo-carotene/docs/3. Others" "workspace/docs/docs/2. Flamingo Carotene/"
-
